3f320658f47f9e443296a47409c95ebd1e6262ad
Peter Shchuchkin <peters@yandex.ru>
2019-08-13 08:38:25 +0300

    moved all functions to editor

--- theme/public/ym.js
+++ theme/public/ym.js
@@ -1,5 +1,6 @@
 $(function(){
   $('.tagger-switch').on('click', toggleTagger);
+
   const Autocomplete = {
     input: null,
     menu: null,
@@ -48,10 +49,12 @@ $(function(){
       }
     },
     addItem(item) {
-      this.editor.addTag(item.data('tagName'), this.menu)
+      this.editor.addTag(item.data('tagName'))
     }
   }
+
   const TagEditor = {
+    $overlay: null,
     $tags: null,
     $input: null,
     $insertPoint: null,
@@ -73,11 +76,11 @@ $(function(){
           .insertBefore(this.$insertPoint);
       })
     },
-    saveTagsToServer(tags) {
+    saveTagsToServer(tags, fileName) {
       $.ajax({
           url: 'http://127.0.0.1:3000/save/',
           type: 'POST',
-          data: JSON.stringify({fileName: filename, tags: tags}),
+          data: JSON.stringify({fileName: fileName, tags: tags}),
           processData: false,
           dataType: 'json',
           contentType: 'application/json',
@@ -93,158 +96,96 @@ $(function(){
 
       return tags;
     },
-    getFileName($el) {
+    getFileName() {
       return this.$tags.data('filepath');
+    },
+    loadTagsFromServer(fileName) {
+      return $.ajax({
+          url: 'http://127.0.0.1:3000/tags/file/',
+          type: 'GET',
+          data: { fileName: fileName },
+          dataType: 'json',
+      });
+    },
+    async initialize() {
+      let tags = await this.loadTagsFromServer(this.getFileName())
+      if (tags.data) {
+        this.clearTags()
+        this.insertTags(tags.data)
+      }
+    },
+    clearTags() {
+      this.$tags.find('.addedTag').each((k, v) => { $(v).remove() })
     },
     create(props) {
       return Object.create(TagEditor, {
+        $overlay: { value: props.$overlay },
         $input: { value: props.$input },
         $insertPoint: { value: props.$insertPoint },
-        $tags: { value: props.$tags }
+        $tags: { value: props.$tags },
+        $closeButton: { value: props.$overlay.find('.tagger-editor-close') },
+        $addButton: { value: props.$overlay.find('.tag-input-add') },
       })
+    },
+    open() {
+      this.$overlay.css('visibility', 'visible');
+      $(document).on('keydown.overlay', function (e) {
+          if (e.key == 'Escape') {
+              closeWindow(overlay);
+          }
+      })
+      this.$closeButton.on('click',  e => {
+          this.close();
+      })
+      this.$overlay.on('click',  e => {
+          if (e.target === this) {
+            closeWindow($(e.target));
+          }
+      })
+      this.$tags.on('click.tagRemove', '.tagRemove', event => {
+        event.preventDefault();
+        $(event.target).parent().remove();
+        this.saveTagsToServer(this.getTags(), this.getFileName())
+      })
+      this.$input.keypress(event => {
+        if (event.which == '13') {
+          this.addTag($(event.target).val())
+          $(event.target).val('')
+        }
+      })
+      this.$addButton.on('click', event => {
+        this.addTag(this.$input.val());
+        this.$input.val('')
+      })
+    },
+    close() {
+      this.$overlay.css('visibility', 'hidden');
+      this.destroy()
+    },
+    destroy() {
+      $(document).off('keydown.overlay')
+      this.$overlay.off('click')
+      this.closeButton.off('click')
+      this.$tags.off('click.tagRemove')
+      this.$input.off('keypress')
+      this.$addButton.off('click')
     }
   }
+
   function toggleTagger(e) {
     var overlay = $(e.target).parents('.captor').children('.tagger-overlay');
-    overlay.css('visibility', 'visible');
-    let input = overlay.find('.tag-input-text')
-    input.focus();
-    let tags = loadTagsFromServer(getFileName(input))
-      .then(tags => {
-        if (tags.data) {
-            clearTags(input)
-            insertTags(tags.data, locateInsertPoint(input))
-        }
-      })
-    $(document).on('keydown.overlay', function (e) {
-        if (e.key == 'Escape') {
-            closeWindow(overlay);
-        }
-    });
     let editor = TagEditor.create({
+      $overlay: overlay,
       $insertPoint: overlay.find('.tags .tagAdd'),
       $input: overlay.find('.tag-input-text'),
       $tags: overlay.find('.tags')
     })
+    editor.initialize()
+    editor.open()
     let suggest = Autocomplete.create({
       menu: overlay.find('.tag-autocomplete-menu'),
       editor: editor
     })
     suggest.updateMenu()
-
-    console.log(e);
   }
-
-  function getTags($el) {
-      let tags = [];
-      $el.find('.addedTag').each(function (idx, li) {
-          if ($(li).data('tagName')) {
-            tags.push($(li).data('tagName'));
-          }
-      });
-
-      return tags;
-  }
-
-  function  getFileName($el) {
-      let filename = '';
-      const container = locateTagsContainer($el);
-      return container.data('filepath');
-  }
-
-  function clearTags(where) { 
-      locateTagsContainer(where).find('.addedTag').each((k, v) => { $(v).remove() })
-  }
-
-  function addTag(value, where) {
-    let tags = getTags(locateTagsContainer(where));
-    if (tags.indexOf(value) !== -1) {
-        return;
-    }
-    insertTags([value], where)
-
-    saveTagsToServer(getTags(locateTagsContainer(where)), getFileName(where));
-    return value;
-  }
-
-  function insertTags(tags, where) {
-      tags.map(value => {
-        $('<li>' + value + '</li>')
-          .addClass('addedTag')
-          .data('tagName', value)
-          .append($('<span>x</span>').addClass('tagRemove'))
-          .insertBefore(where);
-      })
-  }
-
-  function loadTagsFromServer(filename) {
-      return $.ajax({
-          url: 'http://127.0.0.1:3000/tags/file/',
-          type: 'GET',
-          data: { fileName: filename },
-          dataType: 'json',
-      });
-  }
-
-  function saveTagsToServer(tags, filename) {
-      $.ajax({
-          url: 'http://127.0.0.1:3000/save/',
-          type: 'POST',
-          data: JSON.stringify({fileName: filename, tags: tags}),
-          processData: false,
-          dataType: 'json',
-          contentType: 'application/json',
-      });
-  }
-
-  function locateInsertPoint(from) {
-      return from.parents('.tag-editor').find('.tags .tagAdd')
-  }
-
-  function locateTagsContainer(from) {
-      return from.parents('.tagger-overlay').find('.tags')
-  }
-
-  function locateTagIput(from)
-  {
-      return from.parents('.tag-editor').find('.tag-input-text')
-  }
-
-  function closeWindow(from) {
-      var overlay = from;
-      if (!from.hasClass('tagger-overlay')) {
-          overlay = from.parents('.tagger-overlay')
-      }
-      overlay.css('visibility', 'hidden');
-      $(document).off('keydown.overlay');
-  }
-
-  $('.tagger-editor-close').on('click', function (e) {
-      closeWindow($(e.target));
-  });
-
-  $('.tagger-overlay').on('click', function (e) {
-      if (e.target === this) {
-        closeWindow($(e.target));
-      }
-  });
-
-  $('.tags').on('click', '.tagRemove', function (event) {
-    event.preventDefault();
-    var tagsContainer = locateTagsContainer($(this));
-    $(this).parent().remove();
-    saveTagsToServer(getTags(tagsContainer), getFileName(tagsContainer));
-  });
-
-  $('.tag-input-text').keypress(function (event) {
-        if (event.which == '13') {
-        addTag($(this).val(), locateInsertPoint($(this)));
-        $(this).val('');
-    }
-  });
-
-  $('.tag-input-add').click(function (event) {
-    addTag(locateTagIput($(this)).val(), locateInsertPoint($(this)));
-    locateTagIput($(this)).val('');
-  });
 });
